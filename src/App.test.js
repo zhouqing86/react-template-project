be449bd157647a69cbeac6373cc23eeb21efaf53
wendll <27952278@qq.com>
2020-09-10 11:36:05 +0800

    Introduce Enzyme for shallow rendering in unit test and add one task to generate test coverage report

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders learn react link', () => {
+    const wrapper = shallow(<App />);
+    console.log(wrapper.text());
+    expect(wrapper.text()).toEqual(expect.stringContaining('Learn React'));
+  });
 });
